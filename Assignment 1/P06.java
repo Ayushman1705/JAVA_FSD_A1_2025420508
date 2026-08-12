e64dfcaf68bd361ce1a4fa6b517bbc1f0cbf1964
1705Ayushman <ayushmansingchauhan17@gmail.com>
2026-08-12 22:03:45 +0530

    Add question 06

--- Assignment 1/P06.java
+++ Assignment 1/P06.java
@@ -1,4 +1,25 @@
-
+import java.util.Scanner;
 public class P06 {
+	public static void main(String[] argus)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter temp in Celcius");
+		double c = sc.nextDouble();
+		
+		double f = (c*(9/5.0))+32;
+		
+		System.out.println(c+"C = "+ f+"F");
+		
+		System.out.println("Enter temp in Farenheit");
+		double fr = sc.nextDouble();
+		
+		double cel = (fr-32)*(5/9.0);
+		
+		System.out.println(fr+"F ="+cel+"C");
+	
+	    sc.close();
+	}
+	
 
 }
